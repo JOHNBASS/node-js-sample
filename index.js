66c57c849422eca649ec4097f7f11bf6a5d2260a
johnbass <johnbass@zhangzhongdeair.client.tw.trendnet.org>
2017-04-24 14:30:05 +0800

    Demo

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var http = require('http');
+var cool = require('cool-ascii-faces');
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+
+app.set('port', (process.env.PORT || 8080));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+
+	console.log("Got response: " + response.statusCode);
+   console.log("ddos:" + request.url);
+  response.send('Hello World!');
+});
+
+app.get('/index', function(request, response) {
+
+	console.log("Got response: " + response.statusCode);
+   console.log("ddos:" + request.url);
+  	response.render('pages/index');
+});
+
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
+
+
+
+http.get({'host': 'api.ipify.org', 'port': 80, 'path': '/'}, function(resp) {
+  resp.on('data', function(ip) {
+    console.log("My public IP address is: " + ip);
+  });
+});
